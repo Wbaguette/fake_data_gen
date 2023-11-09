83a0a828a9bae8f21b753c03970ed6f6a62badf5
jean-pierre <jeanpierre@derbes.net>
2023-11-08 21:53:14 -0500

    stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import team
 import stadium
 import player
+import stats
+import sponsor
 
 from faker import Faker
 fake = Faker()
@@ -8,11 +10,16 @@ fake = Faker()
 
 def main():
    teams = team.gen_teams()
-   # for t in teams: print(t)
-   # stadiums = stadium.gen_stadiums()
+   for t in teams: print(t)
+   stadiums = stadium.gen_stadiums()
    # for s in stadiums: print(s)
    players = player.gen_players(teams)
-   for p in players: print(p)
+   # for p in players: print(p)
+   statss = stats.gen_statistics(players)
+   # for s in statss: print(s)
+   sponsors = sponsor.gen_sponsors(teams)
+   # for sp in sponsors: print(sp)
+   
 
 
 
